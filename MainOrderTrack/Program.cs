86bb1db923ded2b159b4ad360edcb56d3ffafd13
coleman hallom <challom@stu.clcillinois.edu>
2020-02-18 19:02:28 -0600

    Added unit tests.

---
+++ MainOrderTrack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MainOrderTrack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+
+        Square hey = new Square();
+        
+       
+    }
+    public class Square
+    {
+        public double length;
+        public double width;
+
+        public  double Area(double length, double width)
+        {
+           double  totalArea =  length * width;
+            return totalArea;
+            
+        }
+
+    }
+}
--- MainTestProject/UnitTest1.cs
+++ MainTestProject/UnitTest1.cs
@@ -1,14 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OrderTrackerMain;
+using MainOrderTrack;
+using Xunit;
 
 namespace MainTestProject
 {
     [TestClass]
     public class UnitTest1
     {
+        MainOrderTrack.Square hello = new MainOrderTrack.Square();
         [TestMethod]
+        [Fact]
         public void TestMethod1()
         {
+
+            hello.Area(3.2, 'x');
+            hello.Area(3.4, 2.0);
+            
+            
         }
     }
 }
